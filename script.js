e29de655a598f500132320e279b80ac0db204c51
Liudmyla Ivchenko <95207405+Lysiii2012@users.noreply.github.com>
2024-07-11 17:05:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-class Calculator {  
-    add (a, b) {
-        return a + b
+class Calculator {
+    add(...numbers) {
+        return numbers.reduce((acc, num) => acc + num, 0);
     }
-    subtract (a, b) {
-        return  a - b
+
+    subtract(...numbers) {
+        if (numbers.length === 0) return 0;
+        return numbers.slice(1).reduce((acc, num) => acc - num, numbers[0]);
     }
-    multiply (a, b) {
-        return  a * b
+
+    multiply(...numbers) {
+        if (numbers.length === 0) return 0;
+        return numbers.reduce((acc, num) => acc * num, 1);
     }
-    divide(a, b) {
-        if (b === 0){
-            return alert("На 0 ділити не можна")
-        }
-        return a / b
+
+    divide(...numbers) {
+        if (numbers.length === 0) return 0;
+        return numbers.slice(1).reduce((acc, num) => {
+            if (num === 0) {
+                throw new Error("На 0 ділити не можна");
+            }
+            return acc / num;
+        }, numbers[0]);
     }
 }
 
 
-   const calc = new Calculator();
+const calc = new Calculator();
 
 console.log(calc.add(5, 3)); 
-
 console.log(calc.subtract(10, 4));  
-
 console.log(calc.multiply(3, 6));  
-
 console.log(calc.divide(8, 2));  
