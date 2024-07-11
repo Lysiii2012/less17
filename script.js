d25a36fdbde0d401223a6111e59db216974565da
Liudmyla Ivchenko <95207405+Lysiii2012@users.noreply.github.com>
2024-07-11 12:57:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,10 @@ class Coach {
     displayInfo() {
         console.log(`Coach: ${this.name}, Specialization: ${this.specialization}, Rating: ${this.rating}`)
     } 
-   }
+}
    
-   const coach1 = new Coach('John Doe', 'Fitness', 4.7);
+const coach1 = new Coach('John Doe', 'Fitness', 4.7);
+const coach2 = new Coach('Alice Smith', 'Yoga', 4.9);
    
-   const coach2 = new Coach('Alice Smith', 'Yoga', 4.9);
-   
-   coach1.displayInfo(); 
-   
-   coach2.displayInfo();  
+coach1.displayInfo(); 
+coach2.displayInfo();  
