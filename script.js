6b099583bb4b73860d2f7dd5b3b5a7753312cb05
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2024-07-11 12:48:54 +0300

    res

---
+++ script.js
@@ -0,0 +1,28 @@
+class Calculator {  
+    add (a, b) {
+        return a + b
+    }
+    subtract (a, b) {
+        return  a - b
+    }
+    multiply (a, b) {
+        return  a * b
+    }
+    divide(a, b) {
+        if (b === 0){
+            return alert("На 0 ділити не можна")
+        }
+        return a / b
+    }
+}
+
+
+   const calc = new Calculator();
+
+console.log(calc.add(5, 3)); 
+
+console.log(calc.subtract(10, 4));  
+
+console.log(calc.multiply(3, 6));  
+
+console.log(calc.divide(8, 2));  
